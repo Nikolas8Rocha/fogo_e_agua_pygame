c3822e01ffa07b2e2e28c8a32de175c3be1316c2
Felipe Serra <felipess16@al.insper.edu.br>
2024-11-13 20:05:58 -0300

    PASTAS DA DIVISÃO CRIADAS

---
+++ game.py
@@ -0,0 +1,46 @@
+import pygame
+from constantes import *
+from classes import *
+from utils import *
+
+
+
+# Inicialização do Pygame.
+pygame.init()
+pygame.mixer.init()
+
+# Tamanho da tela.
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+
+# Nome do jogo
+pygame.display.set_caption(TITULO)
+
+# Imprime instruções
+print('*' * len(TITULO))
+print(TITULO.upper())
+print('*' * len(TITULO))
+print('Utilize as setas do teclado para andar e pular.')
+
+# Comando para evitar travamentos.
+try:
+    game_screen(screen)
+finally:
+    pygame.quit()
+
+#LOOP DO JOGO PRINCIPAL:
+def game_screen(screen):
+    
+    state = INIT
+
+    while state != DONE:
+        if state == INIT:
+            screen = pygame.display.set_mode((WIDTH, HEIGHT))
+            state = fog_water_start(screen)
+        elif state == PLAYING:
+            state = fase1(screen)
+        elif state == HOME2:
+            state = fase2(screen)
+        elif state == HOME3:
+            state = fase3(screen)
+        else:
+            state = DONE
